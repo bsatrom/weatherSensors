8e1bb2a9701aa324a289cb1ffb0072373d06f430
Brandon Satrom <bsatrom@gmail.com>
2015-04-24 17:11:50 -0500

    started code for light sensor

--- src/default.js
+++ src/default.js
@@ -1,6 +1,23 @@
 var storage = require('./storage.js');
+var gpio = require('rpi-gpio');
+var lightSensorPin = 17;
+
+function setUp() {
+  gpio.setup(lightSensorPin, gpio.DIR_IN);
+}
+
+setUp();
 
 setInterval(function () {
+  gpio.read(lightSensorPin, function(err, value) {
+    if (err) {
+      console.log('An error ocurred: ' + err);
+      return;
+    }
+
+    console.log('lightValue is: ' +  value);
+  });
+
   var reading = new storage.sensorReading();
   reading.temperature = 77.33;
   reading.location.longitude = 122.11;
